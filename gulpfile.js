d0cb8bb5a40130dfca3e631e8309d602abeb1ded
brandly <brandly13@gmail.com>
2015-01-23 20:10:42 -0500

    got that gulp recipe working

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var source = require('vinyl-source-stream');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var reactify = require('reactify');
+var to5ify = require('6to5ify');
+var rename = require('gulp-rename');
+
+var paths = {
+  dest: 'dist/'
+};
+
+function compileScripts(watch) {
+  gutil.log('Starting browserify');
+
+  var entryFile = './src/app.js';
+  var browserifyOpts = {debug: true, entries: entryFile, cache: {}};
+
+  var bundler;
+  if (watch) {
+    bundler = watchify(browserify(browserifyOpts));
+  } else {
+    bundler = browserify(browserifyOpts);
+  }
+
+  bundler.transform(reactify);
+  bundler.transform(to5ify);
+
+  var rebundle = function () {
+    return bundler.bundle()
+      .on('error', function (err) { console.error(err) })
+      .pipe(source(entryFile))
+      .pipe(rename('app.js'))
+      .pipe(gulp.dest(paths.dest));
+  }
+
+  bundler.on('update', rebundle);
+  return rebundle();
+}
+
+gulp.task('scripts', function () {
+  compileScripts(false);
+});
+
+gulp.task('default', function () {
+  compileScripts(true);
+});
