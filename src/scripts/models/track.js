fd234c949d7477b0783fbd599529be6a4478e9ac
brandly <brandly13@gmail.com>
2015-01-24 03:29:19 -0500

    oh yeah es6

--- src/scripts/models/track.js
+++ src/scripts/models/track.js
@@ -1,8 +1,10 @@
 
-export default function track (opts) {
-  this.id = opts.id || Date.now();
-  this.title = opts.title || '';
-  this.type = opts.type || 'MIDI';
-  this.recordings = opts.recordings || [];
-  this.instrument = opts.instrument || null;
+export default class Track {
+  constructor(opts) {
+    this.id = opts.id || Date.now();
+    this.title = opts.title || '';
+    this.type = opts.type || 'MIDI';
+    this.recordings = opts.recordings || [];
+    this.instrument = opts.instrument || null;
+  }
 }
