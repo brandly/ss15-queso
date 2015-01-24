8b180477cc21c7dd51ff638599c134d5c02274d2
brandly <brandly13@gmail.com>
2015-01-23 22:11:37 -0500

    figure out some basic ES6 syntax and write a lil react module

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
-console.log('ayoooooooo');
+/** @jsx REACT.DOM */
+import React from 'react';
+import APP from './scripts/components/app';
+
+React.render(
+  <APP />,
+  document.getElementById('main')
+);
---
+++ src/scripts/components/app.js
@@ -0,0 +1,13 @@
+/** @jsx REACT.DOM */
+import React from 'react';
+
+export default React.createClass({
+  render: function () {
+    return (
+      <div>
+        <h1>Queso</h1>
+        <p>sup</p>
+      </div>
+    )
+  }
+});
