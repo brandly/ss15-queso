b087d275dd3cb06234af14a36bb1c2d37d5ad1f8
Evan Hahn <me@evanhahn.com>
2015-01-24 18:15:03 -0800

    add keyboard to piano keyboard component

--- src/scripts/components/piano-keyboard.js
+++ src/scripts/components/piano-keyboard.js
@@ -1,10 +1,29 @@
 /** @jsx REACT.DOM */
 import React from 'react';
+import cuid from 'cuid';
 
 export default React.createClass({
+  componentDidMount: function() {
+    let keyboardEl = this.refs.keyboard.getDOMNode();
+    keyboardEl.id = cuid();
+    this.hancock = new QwertyHancock({
+      id: keyboardEl.id,
+      width: 600,
+      height: 150,
+      octaves: 2,
+      startNote: 'A3',
+      whiteNotesColour: 'white',
+      blackNotesColour: 'black',
+      hoverColour: '#f3e939'
+    });
+    setTimeout(() => {
+      console.log('widening');
+      this.hancock.width = 700;
+    }, 5000);
+  },
   render: function () {
     return (
-      <div className="piano-keyboard">TODO keyboard goes here</div>
+      <div ref="keyboard" className="piano-keyboard">TODO keyboard goes here</div>
     );
   }
 });
