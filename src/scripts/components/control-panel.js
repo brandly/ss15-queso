836087fb883370a2bf8c8fd3b6a3166ce9624128
Evan Hahn <me@evanhahn.com>
2015-01-24 18:02:53 -0800

    begin to add keyboard to control panel for midi tracks

--- src/scripts/components/control-panel.js
+++ src/scripts/components/control-panel.js
@@ -1,14 +1,18 @@
 /** @jsx REACT.DOM */
 import React from 'react';
+import PianoKeyboard from './piano-keyboard';
 
 export default React.createClass({
   render: function () {
 
     const trackTitle = this.props.track ? this.props.track.title : 'CONTROL PANEL';
+    const trackType = this.props.track ? this.props.track.type : null;
+    const showKeyboard = trackType === 'MIDI';
 
     return (
       <div className="control-panel">
         <p>{trackTitle}</p>
+        {showKeyboard ? <PianoKeyboard /> : ''}
       </div>
     );
 
---
+++ src/scripts/components/piano-keyboard.js
@@ -0,0 +1,10 @@
+/** @jsx REACT.DOM */
+import React from 'react';
+
+export default React.createClass({
+  render: function () {
+    return (
+      <div className="piano-keyboard">TODO keyboard goes here</div>
+    );
+  }
+});
