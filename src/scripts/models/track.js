1cdd9f40afcf671bdb6da62b11a180cc6485b2a5
brandly <brandly13@gmail.com>
2015-01-24 20:04:18 -0500

    add a panner to the track

--- src/scripts/models/track.js
+++ src/scripts/models/track.js
@@ -11,11 +11,12 @@ export default class Track extends EventEmitter {
     this.instrument = opts.instrument || new Synth();
     // pass in a gain value on creation, or not
     this.gain = tsw.gain(opts.gain || 1.0);
+    this.panner = tsw.panner(opts.panner || 0);
   }
 
   connect() {
     if (this.instrument) {
-      tsw.connect(this.instrument.getNode(), this.gain, tsw.speakers);
+      tsw.connect(this.instrument.getNode(), this.panner, this.gain, tsw.speakers);
     }
   }
 }
